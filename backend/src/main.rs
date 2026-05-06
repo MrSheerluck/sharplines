68aa00645f025db23fc7277762553585b62aafd8
Mr Sheerluck <mrsheerluck003@gmail.com>
2026-05-06 09:41:14 +0530

    hello world

---
+++ backend/src/main.rs
@@ -0,0 +1,29 @@
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "mainline_backend=debug, tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new()
+        .route("/health", get(health_check))
+        .layer(TraceLayer::new_for_http());
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::info!("Server listening on http://{}", addr);
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    async fn health_check() -> &'static str {
+        "OK"
+    }
+}
